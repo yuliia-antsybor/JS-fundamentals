7b5b20a24ce4cc95892d715f3e980aa7827865a1
Yuliia <alice1yulia@gmail.com>
2023-09-12 01:24:18 +0300

    task 2

--- h.w. BOM/2 task/BOM2.js
+++ h.w. BOM/2 task/BOM2.js
@@ -7,3 +7,15 @@
 
 // напишіть функцію changeCSS(), яка спрацьовуватиме по кліку на кнопку і змінюватиме стиль вмісту тега <p>: колір шрифту – оранжевий, розмір шрифту 20px, шрифт сімейства "Comic Sans MS".
 
+
+
+const text = document.getElementById("text");
+const someBtn = document.getElementById("someBtn");
+someBtn.addEventListener("click", changeCSS);
+
+  function changeCSS() {
+    text.style.color = "red";
+    text.style.fontSize = "20px";
+    text.style.fontFamily = "Comic Sans MS";
+  }
+
