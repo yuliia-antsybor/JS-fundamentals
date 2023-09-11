81e2bfd2f88bf68189a70a28acb9453975230359
Yuliia <alice1yulia@gmail.com>
2023-09-11 14:53:04 +0300

    first task

--- h.w. BOM/1 task/BOM1.js
+++ h.w. BOM/1 task/BOM1.js
@@ -4,3 +4,16 @@
 //      2) із затримкою 2 сек змініть розміри вікна на 500х500 пікселів
 //      3) із затримкою 2 сек перемістіть вікно в точку з координатами (200, 200)
 //      4) із затримкою 2 сек закрийте вікно.
+let newWindow = window.open("https://google.com/", "myWindow", "width=300,height=300");
+
+setTimeout(() => {
+  newWindow.resizeTo(500, 500);
+
+  setTimeout(() => {
+    newWindow.moveTo(200, 200);
+
+    setTimeout(() => {
+      newWindow.close();
+    }, 2000);
+  }, 2000);
+}, 2000); 
