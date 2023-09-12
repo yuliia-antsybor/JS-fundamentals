313d4dd10b758acb79e9cd4c296db188a98841e0
Yuliia <alice1yulia@gmail.com>
2023-09-13 00:39:19 +0300

    task 5

--- h.w. BOM/5 task/BOM5.js
+++ h.w. BOM/5 task/BOM5.js
@@ -1,5 +1,22 @@
 
 // Реалізуйте програму, яка по натисканню на кнопку виводитиме повідомлення
-// "I was pressed!", при наведенні на кнопку виводитиме повідомлення "Mouse on me!", а при відведенні курсора миші виводитиме повідомлення "Mouse is not on me!".
+// "I was pressed!", при наведенні на кнопку виводитиме повідомлення "Mouse on me!", а при 
+//відведенні курсора миші виводитиме повідомлення "Mouse is not on me!".
 // Приклад роботи:
 // https://user-images.githubusercontent.com/9075641/228462490-a2ec423a-44e3-40f5-9c0a-7e9d207f2822.png
+
+const button = document.getElementById('button');
+const output = document.getElementById('output');
+
+button.addEventListener('click', () => {
+  output.innerHTML += '<p>I was pressed!</p>'
+});
+
+button.addEventListener('mouseenter', () => {
+  output.innerHTML += '<p>Mouse on me!</p>'
+});
+
+button.addEventListener('mouseleave', () => {
+  output.innerHTML += '<p>Mouse is not on me!</p>'
+});
+
