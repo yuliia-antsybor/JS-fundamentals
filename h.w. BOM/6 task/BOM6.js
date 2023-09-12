763a5c2aaa51bcf5d5fcd3b902976e8542d963a9
Yuliia <alice1yulia@gmail.com>
2023-09-13 01:12:56 +0300

    task 6

--- h.w. BOM/6 task/BOM6.js
+++ h.w. BOM/6 task/BOM6.js
@@ -1,4 +1,10 @@
 
-// Реалізуйте програму, яка відслідковуватиме зміну розміру (ширини і висоти) вікна браузера і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
+// Реалізуйте програму, яка відслідковуватиме зміну розміру (ширини і висоти) вікна браузера і виводитиме на поточну 
+//сторінку при її розтязі/стисканні відповідні значення.
 // https://user-images.githubusercontent.com/9075641/228463011-5302f615-86e6-41dc-b09a-62bed3564596.png
 
+const output = document.getElementById('output');
+window.addEventListener('resize', () => {
+  output.textContent = `${window.innerWidth}x${window.innerHeight}`;
+});
+output.textContent = `${window.innerWidth}x${window.innerHeight}`;
