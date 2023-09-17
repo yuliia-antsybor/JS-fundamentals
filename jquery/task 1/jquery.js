b7a79199292ed13c977f9fb12fe36613843f953e
Yuliia <alice1yulia@gmail.com>
2023-09-17 16:43:39 +0300

    jquery creating folders and input tasks

---
+++ jquery/task 1/jquery.js
@@ -0,0 +1,9 @@
+// 1.
+// Для заданої сторінки знайдіть всі <h2> з класом head, зробіть для них зелений колір фону, потім серед 
+//знайдених елементів знайдіть елементи з класом inner і поставте їм розмір шрифту 35px.
+
+//     <h2 class="head">header1</h2>
+//     <h2 class="head">header2 <span class="inner">inner elem1</span></h2>
+//     <h2>header3</h2>
+//     <h2 class="head">header4<span>inner elem2</span></h2>
+//     <h2>header5</h2>
