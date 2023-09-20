228d36c4a554b85d1d65f534e2ad46f8d2ab23f9
Yuliia <alice1yulia@gmail.com>
2023-09-20 16:39:59 +0200

    solution of task 3

--- jquery/task 3/jquery.js
+++ jquery/task 3/jquery.js
@@ -1,6 +1,23 @@
 // 3. Знайдіть теги <div>, які стоять безпосередньо після <h3> і перемістіть кожен <div>-елемент так, 
 //щоб він став безпосередньо над <h3>.
+
 //    <h3>header1</h3>
 //    <div>text1</div>
 //    <h3>header2</h3>
 //    <div>text2</div>
+
+$(document).ready(function() {
+  // Знаходимо всі теги <h3>
+  var h3Elements = $('h3');
+
+  // Проходимо через кожен тег <h3>
+  h3Elements.each(function(index, element) {
+      // Знаходимо наступний елемент після <h3>, який є <div>
+      var nextDiv = $(element).next('div');
+
+      // Переміщуємо <div> перед <h3>
+      if (nextDiv.length > 0) {
+          $(element).before(nextDiv);
+      }
+  });
+});
